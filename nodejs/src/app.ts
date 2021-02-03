c79bfa3c0786fc4176476dedf4f83fe1d71b3395
Hentie Louw <hentie.louw@ovoenergy.com>
2021-02-03 09:51:15 +0000

    Add Express CORS middleware

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
 // Set up middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
